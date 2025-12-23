4ca5e55cc644716826cfc960b65abbda829d44a3
khammersmith17 <kilianhammersmith@gmail.com>
2025-12-23 16:37:12 -0600

    mb streaming py api

--- python/fair_perf_ml/bias/streaming.py
+++ python/fair_perf_ml/bias/streaming.py
@@ -58,12 +58,12 @@ class DataBiasStreaming(Generic[F, G]):
         self,
         feature_segment_criteria: BiasSegmentationCriteria[F],
         ground_truth_segment_criteria: BiasSegmentationCriteria[G],
-        feautre_data: Iterable[F],
+        feature_data: Iterable[F],
         ground_truth_data: Iterable[G],
     ):
         self._f_seg_criteria = feature_segment_criteria
         self._gt_seg_criteria = ground_truth_segment_criteria
-        labeled_feats = list(map(self._f_seg_criteria.label, feautre_data))
+        labeled_feats = list(map(self._f_seg_criteria.label, feature_data))
         labeled_gt = list(map(self._gt_seg_criteria.label, ground_truth_data))
         self._inner: PyDataBiasStreaming = PyDataBiasStreaming(
             labeled_feats, labeled_gt
@@ -150,6 +150,7 @@ class DataBiasStreaming(Generic[F, G]):
     def performance_snapshot(self) -> Dict[str, float]:
         """
         Generate a performance snapshot, irrespective of the baseline data state.
+        An exception will be thrown if no runtime data has been pushed into the stream.
         """
         report: Dict[str, float] = self._inner.performance_snapshot()
         return report
@@ -158,6 +159,154 @@ class DataBiasStreaming(Generic[F, G]):
         """
         Generate a drift report, detailing the drift from the baseline state observed
         in the runtime data stream.
+        An exception will be thrown if no runtime data has been pushed into the stream.
         """
         report: DriftReport = self._inner.drift_report()
+        return report
+
+
+class ModelBiasStreaming(Generic[F, P, G]):
+    __slots__ = ["_inner", "_f_seg_criteria", "_p_seg_criteria", "_gt_seg_criteria"]
+
+    def __init__(
+        self,
+        feature_segment_criteria: BiasSegmentationCriteria[F],
+        ground_truth_segment_criteria: BiasSegmentationCriteria[G],
+        prediction_segment_criteria: BiasSegmentationCriteria[P],
+        feature_data: Iterable[F],
+        ground_truth_data: Iterable[G],
+        prediction_data: Iterable[P],
+    ):
+        self._f_seg_criteria = feature_segment_criteria
+        self._p_seg_criteria = prediction_segment_criteria
+        self._gt_seg_criteria = ground_truth_segment_criteria
+
+        labeled_feats = list(map(self._f_seg_criteria.label, feature_data))
+        labeled_gt = list(map(self._gt_seg_criteria.label, ground_truth_data))
+        labeled_preds = list(map(self._p_seg_criteria.label, prediction_data))
+        self._inner: PyModelBiasStreaming = PyModelBiasStreaming(
+            labeled_feats, labeled_preds, labeled_gt
+        )
+
+    def push(self, feature: F, prediction: P, ground_truth: G) -> None:
+        """
+        Push a single feature, prediction, and ground truth example into the stream.
+        Type checkers will enforce that the type passed for each value matches the type
+        defined in the segmentation criteria.
+        args:
+            feature: F
+            prediction: P
+            ground_truth: G
+        returns:
+            None
+        """
+        self._inner.push(
+            self._f_seg_criteria.label(feature),
+            self._p_seg_criteria.label(prediction),
+            self._gt_seg_criteria.label(ground_truth),
+        )
+
+    def push_batch(
+        self,
+        feature_data: Iterable[F],
+        ground_truth_data: Iterable[G],
+        prediction_data: Iterable[P],
+    ) -> None:
+        """
+        Push a single feature, prediction, and ground truth example into the stream.
+        Type checkers will enforce that the type passed for each value matches the type
+        defined in the segmentation criteria. The iterables/arrays passed should all be of
+        the same length. If either invariant is violated, then an excpetion will be thrown.
+
+        args:
+            feature: Iterable[F]
+            prediction: Iterable[P]
+            ground_truth: Iterable[G]
+        returns:
+            None
+        """
+        labeled_feats = list(map(self._f_seg_criteria.label, feature_data))
+        labeled_gt = list(map(self._gt_seg_criteria.label, ground_truth_data))
+        labeled_preds = list(map(self._p_seg_criteria.label, prediction_data))
+
+        self._inner.push(
+            labeled_feats,
+            labeled_preds,
+            labeled_gt,
+        )
+
+    def reset_baseline(
+        self,
+        feature_data: Iterable[F],
+        ground_truth_data: Iterable[G],
+        prediction_data: Iterable[P],
+    ) -> None:
+        """
+        Resets the baseline state with using a new baseline dataset. This will leverage
+        the same segmentation criteria defined at type construction.
+
+        args:
+            feature: Iterable[F]
+            prediction: Iterable[P]
+            ground_truth: Iterable[G]
+        returns:
+            None
+        """
+        labeled_feats = list(map(self._f_seg_criteria.label, feature_data))
+        labeled_gt = list(map(self._gt_seg_criteria.label, ground_truth_data))
+        labeled_preds = list(map(self._p_seg_criteria.label, prediction_data))
+        self._inner.reset_baseline(labeled_feats, labeled_preds, labeled_gt)
+
+    def reset_baseline_and_segmentation_criteria(
+        self,
+        feature_segment_criteria: BiasSegmentationCriteria[F],
+        ground_truth_segment_criteria: BiasSegmentationCriteria[G],
+        prediction_segment_criteria: BiasSegmentationCriteria[P],
+        feature_data: Iterable[F],
+        ground_truth_data: Iterable[G],
+        prediction_data: Iterable[P],
+    ) -> None:
+        """
+        Resets the baseline state with using a new baseline dataset. This is the only that allows
+        a change in the segmentation criteria used for class segmentation. A reset in the
+        baseline set is required, to avoid inconsistent state in runtime class bucketing.
+        Type checkers will enforce the same type to be used in the segmentation criteria.
+
+        args:
+            feature: Iterable[F]
+            prediction: Iterable[P]
+            ground_truth: Iterable[G]
+        returns:
+            None
+        """
+        self._f_seg_criteria = feature_segment_criteria
+        self._gt_seg_criteria = ground_truth_segment_criteria
+        self._p_seg_criteria = prediction_segment_criteria
+
+        labeled_feats = list(map(self._f_seg_criteria.label, feature_data))
+        labeled_gt = list(map(self._gt_seg_criteria.label, ground_truth_data))
+        labeled_preds = list(map(self._p_seg_criteria.label, prediction_data))
+        self._inner.reset_baseline(labeled_feats, labeled_preds, labeled_gt)
+
+    def flush(self) -> None:
+        """
+        Clear all runtime state.
+        """
+        self._inner.flush()
+
+    def performance_snapshot(self) -> Dict[str, float]:
+        """
+        Generate a performance snapshot of runtime state, irrespective of the baseline state.
+        An exception will be thrown if no runtime data has been pushed into the stream.
+        """
+        report: Dict[str, float] = self._inner.performance_snapshot()
+        return report
+
+    def drift_snapshot(self) -> DriftReport:
+        """
+        Generate a drift report, detailing the drift from the baseline state observed
+        in the runtime data stream.
+        An exception will be thrown if no runtime data has been pushed into the stream.
+        """
+        report: DriftReport = self._inner.drift_snapshot()
         return report
