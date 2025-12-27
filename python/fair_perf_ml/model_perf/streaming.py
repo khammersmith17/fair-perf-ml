90d24c89246c725dc8b90641a2f16b921e4de73e
khammersmith17 <kilianhammersmith@gmail.com>
2025-12-27 15:34:45 -0600

    add doc comments perf streaming py

--- python/fair_perf_ml/model_perf/streaming.py
+++ python/fair_perf_ml/model_perf/streaming.py
@@ -1,3 +1,15 @@
+"""
+This module contains streaming containers for the model performance
+utilities found in this library. The streaming implemenations are implemented
+in the rust crate and expose Python interfaces used here.
+
+The streaming utilities are meant for long running services, and provide
+a space and computationally efficient way to observe model performance for
+an ML service. Snapshots are provided that perform snapshot analysis of model
+performance at a point in time. There are utilities to update on the fly when
+there is a model update, or data drifts, without needing to pull down the service.
+"""
+
 from typing import Protocol, Generic, TypeVar, Self, Iterable, Dict, Optional
 from .._fair_perf_ml import (
     PyBinaryClassificationStreaming,
@@ -21,7 +33,7 @@ L = TypeVar("L", bound=LabelBounds)
 
 class BinaryClassificationStreaming(Generic[L]):
     """
-    Wrapper around core rust logic to provide a Pythonic api to users.
+    Streaming variant of the binary classification utilities.
     Requires a label, which represents the positive class label. This
     label can be any type as long as there is an explicit __eq__ implementation.
     The label will be applied in Python before the labeled data is passed to the
@@ -124,35 +136,81 @@ class BinaryClassificationStreaming(Generic[L]):
         return report
 
 
-# TODO: doc strings
 class LinearRegressionStreaming:
+    """
+    Streaming variant of the linear regression monitoring provided in this library.
+    """
+
     __slots__ = ["_inner"]
 
     def __init__(self, y_true: Iterable[float], y_pred: Iterable[float]):
+        """
+        Contructs a streaming client and defines initial baseline state.
+        args:
+            y_true: Iterable[float]
+            y_pred: Iterable[float]
+        """
         self._inner = PyLinearRegressionStreaming(y_true, y_pred)
 
     def push(self, y_true: float, y_pred: float) -> None:
+        """
+        Push a single ground truth/prediction example.
+        args:
+            y_true: float
+            y_pred: float
+        """
         self._inner.push(y_true, y_pred)
 
     def push_batch(self, y_true: Iterable[float], y_pred: Iterable[float]) -> None:
+        """
+        Push a batch of ground truth/prediction examples.
+        args:
+            y_true: Iterable[float]
+            y_pred: Iterable[float]
+        """
         self._inner.push_batch(y_true, y_pred)
 
     def reset_baseline(self, y_true: Iterable[float], y_pred: Iterable[float]) -> None:
+        """
+        Reset the baseline state on with a new baseline dataset.
+        args:
+            y_true: Iterable[float]
+            y_pred: Iterable[float]
+        """
         self._inner.reset_baseline(y_true, y_pred)
 
     def flush(self) -> None:
+        """
+        Flush and clear the runtime state.
+        """
         self._inner.flush()
 
     def performance_snapshot(self) -> Dict[str, float]:
+        """
+        Generate a point in time performance snapshot, irrespective of
+        the baseline set. This will throw an exception is the runtime
+        state is empty.
+        returns:
+            Dict[str, float] - metric name: metric pair
+        """
         report: Dict[str, float] = self._inner.performance_snapshot()
         return report
 
     def drift_snapshot(self) -> DriftReport:
+        """
+        Generate a point in time drift snapshot, with respect to the baseline
+        state. This will throw and exception when the runtime state is
+        empty.
+        """
         report: DriftReport = self._inner.drift_snpshot()
         return report
 
 
 class LogisticRegressionStreaming:
+    """
+    Streaming variant of the logisitic regression utilities.
+    """
+
     __slots__ = ["_inner"]
 
     def __init__(
@@ -161,31 +219,82 @@ class LogisticRegressionStreaming:
         y_pred: Iterable[float],
         threshold: Optional[float] = 0.5,
     ):
+        """
+        Constructs a streaming agent with a baseline dataset and an optional decision
+        threshold.
+        args:
+            y_true: Iterable[float]
+            y_pred: Iterable[float]
+            threshold: Optional[float] = 0.5
+        """
         self._inner: PyLogisticRegressionStreaming = PyLogisticRegressionStreaming(
             y_true, y_pred, threshold
         )
 
     def push(self, y_true: float, y_pred: float) -> None:
+        """
+        Push a single ground truth/prediction example.
+        args:
+            y_true: float
+            y_pred: float
+        """
         self._inner.push(y_true, y_pred)
 
     def push_batch(self, y_true: Iterable[float], y_pred: Iterable[float]) -> None:
+        """
+        Push a batch of ground truth/prediction examples.
+        args:
+            y_true: Iterable[float]
+            y_pred: Iterable[float]
+        """
         self._inner.push_batch(y_true, y_pred)
 
     def flush(self) -> None:
+        """
+        Flush and clear the runtime state.
+        """
         self._inner.flush()
 
     def reset_baseline(self, y_true: Iterable[float], y_pred: Iterable[float]):
+        """
+        Reset the baseline state on with a new baseline dataset. The decision threshold
+        is not changed.
+        args:
+            y_true: Iterable[float]
+            y_pred: Iterable[float]
+        """
         self._inner.reset_baseline(y_true, y_pred)
 
     def reset_baseline_and_decision_threshold(
         self, y_true: Iterable[float], y_pred: Iterable[float], threshold: float
     ) -> None:
+        """
+        Reset the baseline state on with a new baseline dataset. The decision threshold
+        is updated here, and as such, a new baseline is required to update the decision
+        threhold to maintain consistent state.
+        args:
+            y_true: Iterable[float]
+            y_pred: Iterable[float]
+            threshold: float
+        """
         self._inner.reset_baseline_and_decision_threshold(y_true, y_pred, threshold)
 
     def performance_snapshot(self) -> Dict[str, float]:
+        """
+        Generate a point in time performance snapshot, irrespective of
+        the baseline set. This will throw an exception is the runtime
+        state is empty.
+        returns:
+            Dict[str, float] - metric name: metric pair
+        """
         report: Dict[str, float] = self._inner.performance_report()
         return report
 
     def drift_snapshot(self) -> DriftReport:
+        """
+        Generate a point in time drift snapshot, with respect to the baseline
+        state. This will throw and exception when the runtime state is
+        empty.
+        """
         report: DriftReport = self._inner.drift_snapshot()
         return report
