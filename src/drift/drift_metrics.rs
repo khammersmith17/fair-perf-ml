b73f4f7d5793ccec80cc6a457e81cf5066e79b78
khammersmith17 <kilianhammersmith@gmail.com>
2026-01-09 16:20:05 -0500

    base of jensen shannon fro drift

--- src/drift/drift_metrics.rs
+++ src/drift/drift_metrics.rs
@@ -46,22 +46,52 @@ pub(crate) fn compute_kl_divergence_drift(
         .sum()
 }
 
+#[inline]
+pub(crate) fn compute_jenson_shannon_divergence_drift(
+    baseline_hist: &[f64],
+    runtime_bins: &[f64],
+    n: f64,
+) -> f64 {
+    // validate that rt and baseline bins are of same length
+    debug_assert_eq!(runtime_bins.len(), baseline_hist.len());
+    let eps = *STABILITY_EPS;
+
+    let mut js = 0_f64;
+    let half_fac = 0.5_f64;
+
+    for (bl, rt) in baseline_hist.iter().zip(runtime_bins.iter()) {
+        let p = (bl + eps).max(eps);
+        let q = ((rt / n) + eps).max(eps);
+        let m = (p + q) * half_fac;
+
+        js += (half_fac * p * (p / m).ln()) + (half_fac * q * (q / m).ln());
+    }
+
+    js / 2_f64.ln()
+}
+
+#[allow(unused)]
+use super::data_drift::CategoricalDataDrift;
+#[allow(unused)]
+use super::data_drift::ContinuousDataDrift;
+
 /// KL Divergence implementation for streaming types. Bringing this trait into scope will provided
 /// the implementation for the associated streaming type.
 pub trait StreamingKlDivergenceDrift {
     fn kl_divergence_drift(&self) -> Result<f64, DriftError>;
 }
 
-/// Provides KL Divergence implementation for `ContinuousDataDrift`.
+/// Provides KL Divergence implementation for [`ContinuousDataDrift`].
 pub trait ContinuousKlDivergenceDrift {
     fn kl_divergence_drift(&mut self, runtime_slice: &[f64]) -> Result<f64, DriftError>;
 }
 
-/// Provides KL Divergence implementation for `CategoricalDataDrift`.
+/// Provides KL Divergence implementation for [`CategoricalDataDrift`].
 pub trait CategoricalKlDivergenceDrift {
-    fn kl_divergence_drift<S>(&mut self, runtime_slice: &[S]) -> Result<f64, DriftError>
-    where
-        S: StringLike;
+    fn kl_divergence_drift<S: StringLike>(
+        &mut self,
+        runtime_slice: &[S],
+    ) -> Result<f64, DriftError>;
 }
 
 /// Population Stability Index implementation for streaming types. Bringing this trait into scope will provided
@@ -70,14 +100,27 @@ pub trait StreamingPopulationStabilityIndexDrift {
     fn psi_drift(&self) -> Result<f64, DriftError>;
 }
 
-/// Provides Population Stability Index implementation for `ContinuousDataDrift`.
+/// Provides Population Stability Index implementation for [`ContinuousDataDrift`].
 pub trait ContinuousPSIDrift {
     fn psi_drift(&mut self, runtime_slice: &[f64]) -> Result<f64, DriftError>;
 }
 
-/// Provides Population Stability Index implementation for `CategoricalDataDrift`.
+/// Provides Population Stability Index implementation for [`CategoricalDataDrift`].
 pub trait CategoricalPSIDrift {
-    fn psi_drift<S>(&mut self, runtime_slice: &[S]) -> Result<f64, DriftError>
-    where
-        S: StringLike;
+    fn psi_drift<S: StringLike>(&mut self, runtime_slice: &[S]) -> Result<f64, DriftError>;
+}
+
+/// Provides the implementation of Jensen-Divergence drift for streaming drift types.
+pub trait StreamingJensenShannonDivergenceDrift {
+    fn js_drift(&self) -> f64;
+}
+
+/// Provides the implementation of Jensen-Divergence drift for [`ContinuousDataDrift`].
+pub trait ContinuousJensenShannonDivergenceDrift {
+    fn js_drift(&self, runtime_slice: &[f64]) -> f64;
+}
+
+/// Provides the implementation of Jensen-Divergence drift for [`CategoricalDataDrift`].
+pub trait CategoricalJensenShannonDivergenceDrift {
+    fn js_drift<S: StringLike>(&self, runtime_slice: &[S]) -> f64;
 }
