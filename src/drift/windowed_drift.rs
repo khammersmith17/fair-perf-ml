093663f24524f4f68ceca9f54fe29b84f3a53c34
khammersmith17 <kilianhammersmith@gmail.com>
2026-04-12 12:49:44 -0400

    minimal scaffold

--- src/drift/windowed_drift.rs
+++ src/drift/windowed_drift.rs
@@ -3,9 +3,11 @@ use super::{
     distribution::QuantileType,
 };
 use crate::errors::DriftError;
+use std::env;
 use std::fs::File;
 use std::io::{self, Read, Seek, SeekFrom, Write};
 use std::num::NonZeroUsize;
+use std::sync::OnceLock;
 use tempfile::NamedTempFile;
 /*
 * The goal with this module is to allow for a smooth transition between drift windows.
@@ -33,6 +35,45 @@ use tempfile::NamedTempFile;
 * I think that spilling to disk should happen internally, and dynamically. There should be some
 * threshold where the cached windows should be spilled to disk. Could this be available process
 * memory relative to dataset size, it num items * size >= some threshold.
+*
+*
+* Implementation details
+* - increment epoch counter
+*   - allows for easy is saturated computation and current buffer offset
+* - Keep live window count
+*   - increment live window count every push
+*   - when window is saturated, forward backend pointer
+* - When forwarding epoch pointer
+*   - call evict window every time
+*   - within the evict window method, take in mut global window
+*   - if buffer is not saturated, then it is a no op
+*   - when it is saturated, clear the evicted window from backend buffer
+*   - zero out live window
+* - the backend owns the live window
+* - outer type owns the global window
+* - methods needed on backend
+*   - push
+*       - internally handles backend ops when windows roll
+*       - when saturated
+*           - calls into flush window, flush window from global
+*           - evicts stale window
+*
+*   - epoch_offset
+*       - compute current epoch offset
+*       - window offset in buffer
+*   - bin_offset
+*       - only on in memory backend
+*       - compute offset into buffer of bin based on epoch offset and bin offset
+*   - is_saturated
+*       - determine if entire backend buffer has been saturated
+*   - forward_offset
+*       - increment epoch offset pointer
+*   - offset_tail
+*       - get the tail window
+*   - live_window
+*       - get a ref to the live window
+*       - for in memory backend this will be the slab for the current window
+*       - a seperate live window will be stored for the disk backed buffer
 * */
 
 /*
@@ -42,16 +83,86 @@ use tempfile::NamedTempFile;
 * the current bucket is represented by head.
 * */
 
-enum WindowBuffer {
-    Memory(InMemoryWindowBuffer),
-    Disk,
+static DISK_SPILL_THRESHOLD: OnceLock<usize> = OnceLock::new();
+const DISK_SPILL_THRESHOLD_ENV_VAR: &'static str = "FAIR_PERF_ML_DRIFT_DISK_SPILL_THRESHOLD";
+const DEFAULT_DISK_SPILL_THRESHOLD: usize = 64 * 1000 * 1000; // 64 MB
+
+fn get_spill_heuristic() -> usize {
+    *DISK_SPILL_THRESHOLD.get_or_init(|| {
+        let Ok(raw_env) = env::var(DISK_SPILL_THRESHOLD_ENV_VAR) else {
+            return DEFAULT_DISK_SPILL_THRESHOLD;
+        };
+        let Ok(parsed_thres) = raw_env.parse::<usize>() else {
+            return DEFAULT_DISK_SPILL_THRESHOLD;
+        };
+        parsed_thres
+    })
 }
 
-struct DiskBuffer {
+pub trait WindowCoreBackend {
+    // Evict a stale window from the buffer. Update global state window.
+    // If buffer is not saturated, this is a no-op.
+    fn evict_window(&mut self, global_window: &mut [f64]);
+
+    // Flush the live local window to the buffer.
+    fn flush_window(&mut self, global_window: &mut [f64]);
+
+    // Current window offset
+    fn epoch_offset(&self) -> usize;
+
+    // Flag for if the entire buffer has been saturated
+    fn is_saturated(&self) -> bool;
+
+    // Push the head pointer.
+    fn forward_offset(&mut self);
+
+    // Get a ref to the current live window.
+    fn live_window(&self) -> &[f64];
+
+    // Push into the live window and global window.
+    fn push(&mut self, bin: usize, global_window: &mut [f64]);
+}
+
+struct DiskBackedBackend {
     mem_buf: Vec<u8>,
     disk_buf: NamedTempFile,
     window_size: usize,
     num_epochs: usize,
+    live_window: Vec<f64>,
+}
+
+impl WindowCoreBackend for DiskBackedBackend {
+    fn evict_window(&mut self, global_window: &mut [f64]) {
+        if !self.is_saturated() {
+            return;
+        }
+    }
+
+    fn flush_window(&mut self, global_window: &mut [f64]) {
+        todo!()
+    }
+
+    fn epoch_offset(&self) -> usize {
+        todo!()
+    }
+
+    #[inline]
+    fn is_saturated(&self) -> bool {
+        todo!()
+    }
+
+    #[inline]
+    fn forward_offset(&mut self) {
+        todo!()
+    }
+
+    fn live_window(&self) -> &[f64] {
+        &self.live_window
+    }
+
+    fn push(&mut self, bin: usize, global_window: &mut [f64]) {
+        todo!()
+    }
 }
 
 // after buffer saturation...
@@ -59,167 +170,151 @@ struct DiskBuffer {
 // read in the buffer to be evicted, resolve global state,
 // then clear the live window
 
-impl DiskBuffer {
-    fn new(window_size: usize, num_epochs: usize) -> std::io::Result<DiskBuffer> {
+impl DiskBackedBackend {
+    fn new(window_size: usize, num_epochs: usize) -> std::io::Result<DiskBackedBackend> {
         let disk_buf = NamedTempFile::new()?;
         let mem_buf_size = window_size * num_epochs * 8_usize;
-        Ok(DiskBuffer {
+        Ok(DiskBackedBackend {
             disk_buf,
             mem_buf: vec![0_u8; mem_buf_size],
             window_size: window_size.into(),
             num_epochs: num_epochs.into(),
+            live_window: vec![0_f64; window_size],
         })
     }
-    fn flush_window(&mut self, window: &[f64]) -> std::io::Result<()> {
-        debug_assert_eq!(window.len(), self.window_size);
-
-        // When we reach the end "wrap around" by seeking back to the start.
-        // This maintains a "static size" disk buffer.
-        if self.disk_buf.stream_position()? == self.disk_buf_size() {
-            self.disk_buf.seek(SeekFrom::Start(0))?;
-        }
-        let bytes_written = self.disk_buf.write(bytemuck::cast_slice(window))?;
-        debug_assert_eq!(bytes_written, (std::mem::size_of::<f64>() * window.len()));
-        Ok(())
-    }
-
-    fn read_window_to_evict(&mut self) -> std::io::Result<()> {
-        self.disk_buf.read(&mut self.mem_buf)?;
-        Ok(())
-    }
-
-    fn disk_buf_size(&self) -> u64 {
-        (self.window_size * self.num_epochs) as u64 * 8_u64
-    }
-
-    fn get_evicted_window(&mut self) -> &[f64] {
-        bytemuck::cast_slice(self.mem_buf.as_slice())
-    }
 }
 
-struct DiskWindowBuffer {
-    window_buffer: DiskBuffer,
-    live_window: Vec<f64>,
-}
-
-struct InMemoryWindowBuffer {
+struct InMemoryWindowBackend {
     window_buffer: Vec<f64>, // contiguous allocation of all the windows
     epoch_count: usize,      // the "index" of the current epoch
-    curr_epoch_sat: usize,   // the current saturation of the current epoch
+    epoch_inc: usize,        // the current saturation of the current epoch
     epoch_size: usize,       // size of a single window epoch
+    live_ex_count: usize,
+}
+
+impl WindowCoreBackend for InMemoryWindowBackend {
+    fn evict_window(&mut self, global_window: &mut [f64]) {
+        // Remove the examples captured in the global buffer that are to be flushed.
+        debug_assert!(self.is_saturated());
+        let offset_start = self.window_base_offset();
+        let offset_end = offset_start + self.epoch_size;
+        for i in offset_start..offset_end {
+            global_window[i - offset_start] -= self.window_buffer[i];
+        }
+    }
+
+    fn flush_window(&mut self, global_window: &mut [f64]) {
+        // Flush window in window buffer.
+        // Evict the window from global buffer.
+        if self.is_saturated() {
+            self.evict_window(global_window);
+
+            let offset_start = self.window_base_offset();
+            let offset_end = offset_start + self.epoch_size;
+            for i in offset_start..offset_end {
+                self.window_buffer[i] = 0_f64;
+            }
+        }
+        self.forward_offset();
+    }
+
+    fn epoch_offset(&self) -> usize {
+        self.epoch_inc % self.epoch_count
+    }
+
+    fn is_saturated(&self) -> bool {
+        self.epoch_inc >= self.epoch_count
+    }
+
+    fn forward_offset(&mut self) {
+        self.epoch_inc += 1;
+    }
+
+    fn live_window(&self) -> &[f64] {
+        let offset_start = self.window_base_offset();
+        &self.window_buffer[offset_start..offset_start + self.epoch_size]
+    }
+
+    fn push(&mut self, bin: usize, global_window: &mut [f64]) {
+        if self.live_ex_count == self.epoch_size {
+            self.flush_window(global_window)
+        }
+        let bin_offset = self.window_base_offset() + bin;
+        global_window[bin] += 1_f64;
+        self.window_buffer[bin_offset] += 1_f64;
+    }
 }
 
 struct ConsumedWindow(bool);
 
-impl InMemoryWindowBuffer {
-    fn new(epoch_size: usize, num_bins: usize, num_windows: usize) -> InMemoryWindowBuffer {
-        let window_buffer = vec![0_f64; num_windows * num_bins];
+impl InMemoryWindowBackend {
+    fn new(epoch_size: usize, epoch_count: usize) -> InMemoryWindowBackend {
+        let window_buffer = vec![0_f64; epoch_size * epoch_count];
 
-        InMemoryWindowBuffer {
+        InMemoryWindowBackend {
             window_buffer,
-            epoch_count: 0,
-            curr_epoch_sat: 0,
+            epoch_count,
+            epoch_inc: 0,
             epoch_size,
+            live_ex_count: 0,
         }
-    }
-
-    fn num_epochs(&self) -> usize {
-        debug_assert!(self.window_buffer.len() % self.epoch_size == 0);
-        self.window_buffer.len() / self.epoch_size
-    }
-
-    fn compute_offset(&self, bin_idx: usize) -> usize {
-        (self.epoch_offset() * self.epoch_size) + bin_idx
     }
 
     #[inline]
-    fn push(&mut self, bin_idx: usize) -> ConsumedWindow {
-        let bin_offset = self.compute_offset(bin_idx);
-        self.window_buffer[bin_offset] += 1_f64;
-        ConsumedWindow(self.curr_epoch_sat == self.epoch_size)
-    }
-
-    fn is_saturated(&self) -> bool {
-        self.epoch_count > self.num_epochs()
-    }
-
-    fn epoch_offset(&self) -> usize {
-        self.epoch_count % self.num_epochs()
-    }
-
-    /// Push the head epoch forward. When the buffer has been saturated, then clean the global
-    /// window and clean up the epoch being evicted, as it is now the current epoch to be used.
-    fn forward_head(&mut self) {
-        self.epoch_count += 1;
-    }
-
-    fn roll_window(&mut self, global_bins: &mut [f64]) {
-        self.forward_head();
-        if self.is_saturated() {
-            self.clean_window(global_bins);
-        }
-        self.curr_epoch_sat = 0;
-    }
-
-    fn tail(&self) -> usize {
-        debug_assert!(!self.is_saturated());
-        (self.curr_epoch_sat) + 1 % self.num_epochs()
-    }
-
-    fn clean_window(&mut self, global_bins: &mut [f64]) {
-        let size = self.num_epochs();
-
-        // start offset at the base of the window to be evicted.
-        let base_window_offset = self.epoch_offset() * size;
-        for i in 0..self.epoch_size {
-            let buffer_offset = base_window_offset + i;
-            global_bins[i] -= self.window_buffer[buffer_offset];
-            self.window_buffer[buffer_offset] = 0_f64;
-        }
+    fn window_base_offset(&self) -> usize {
+        self.epoch_size * self.epoch_offset()
     }
 }
 
-pub struct WindowedDataConfig {
+pub struct ContinuousWindowedDriftConfig {
     pub epoch_size: NonZeroUsize,
-    pub num_epochs: NonZeroUsize,
+    pub epoch_count: NonZeroUsize,
     pub quantile_type: Option<QuantileType>,
 }
 
-pub struct WindowedContinuousDirft {
+pub struct CategoricalWindowedDriftConfig {
+    pub epoch_size: NonZeroUsize,
+    pub epoch_count: NonZeroUsize,
+}
+
+pub struct WindowedContinuousDrift<S: WindowCoreBackend> {
     global_bins: Vec<f64>,
-    windowed_buckets: InMemoryWindowBuffer,
+    backend: S,
     baseline_bins: BaselineContinuousBins,
 }
 
-impl WindowedContinuousDirft {
-    pub fn new(
-        config: WindowedDataConfig,
+impl WindowedContinuousDrift<InMemoryWindowBackend> {
+    pub fn new_in_memory_backend(
+        config: ContinuousWindowedDriftConfig,
         baseline_dataset: &[f64],
-    ) -> Result<WindowedContinuousDirft, DriftError> {
-        let WindowedDataConfig {
+    ) -> Result<WindowedContinuousDrift<InMemoryWindowBackend>, DriftError> {
+        let ContinuousWindowedDriftConfig {
             epoch_size,
-            num_epochs,
+            epoch_count,
             quantile_type,
         } = config;
         let baseline_bins =
             BaselineContinuousBins::new(baseline_dataset, quantile_type.unwrap_or_default())?;
-        todo!()
+        let backend = InMemoryWindowBackend::new(epoch_size.into(), epoch_count.into());
+        Ok(WindowedContinuousDrift {
+            global_bins: vec![0_f64; epoch_size.into()],
+            baseline_bins,
+            backend,
+        })
     }
+}
 
+impl<S: WindowCoreBackend> WindowedContinuousDrift<S> {
     #[inline]
-    pub fn push(&mut self, runtime_example: f64) {
-        let bin_idx = self.baseline_bins.resolve_bin(runtime_example);
-        self.global_bins[bin_idx] += 1_f64;
-        let ConsumedWindow(window_consumed) = self.windowed_buckets.push(bin_idx);
-
-        if window_consumed {
-            self.windowed_buckets.roll_window(&mut self.global_bins)
-        }
+    pub fn push(&mut self, example: f64) {
+        let bin = self.baseline_bins.resolve_bin(example);
+        self.backend.push(bin, &mut self.global_bins)
     }
 
-    pub fn push_batch(&mut self, runtime_dataset: &[f64]) {
-        for example in runtime_dataset {
-            self.push(*example)
+    pub fn push_batch(&mut self, examples: &[f64]) {
+        for ex in examples {
+            let bin = self.baseline_bins.resolve_bin(*ex);
+            self.backend.push(bin, &mut self.global_bins)
         }
     }
 }
